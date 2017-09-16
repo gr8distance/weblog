f0428528eccbc4b9a332027d89b9daff7353c65c
UG <suzaku622@gmail.com>
2017-09-17 04:00:03 +0900

    adds header in top page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :title
   protect_from_forgery with: :exception
+
+  private
+
+  def title
+    @title ||= 'Weblog'
+  end
 end
