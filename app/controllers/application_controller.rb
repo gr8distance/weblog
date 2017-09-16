bc131d78cd20ec4060f68edf26f17c349b12cadb
UG <suzaku622@gmail.com>
2017-09-17 04:41:23 +0900

    adds sign in function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,19 @@
 class ApplicationController < ActionController::Base
   before_action :title
+  before_action :configure_sign_up_params, only: [:create]
   protect_from_forgery with: :exception
 
   private
 
   def title
     @title ||= 'Weblog'
+  end
+
+  # def configure_permitted_parameters
+  #   devise_parameter_sanitizer.for(:sign_up) << :name
+  # end
+
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :admin])
   end
 end
