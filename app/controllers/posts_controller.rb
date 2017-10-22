ad8752ab6df3f5d13a9bcff2d52086dcadde61ef
UG <suzaku622@gmail.com>
2017-10-22 23:03:55 +0900

    記事読めるようにする

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
-  before_action :not_admin
+  before_action :not_admin, except: :show
 
   # GET /posts
   # GET /posts.json
