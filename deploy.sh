d6a77d3d203737f8a925af9f31445dd89e7f01a9
UG <suzaku622@gmail.com>
2017-10-23 23:11:52 +0900

    AssetPipelineをシンプルにする

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-docker-compose stop
-docker-compose up --build -d
-
 docker-compose exec app bundle exec rake assets:precompile RAILS_ENV=production
-docker-compose exec app touch tmp/restart.txt
